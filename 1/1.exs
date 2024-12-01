48e678e089a91ecbe06485340d97e73b332a724f
Kaleb Allmon <kalebdallmon@gmail.com>
2024-12-01 15:06:52 -0500

    1.1

---
+++ 1/1.exs
@@ -0,0 +1,24 @@
+{:ok, contents} = File.read("1.txt")
+
+lines = String.split(contents, "\n", trim: true)
+
+# Split the lines into separate lists
+{list1, list2} = Enum.reduce(lines, {[], []}, fn line, {list1, list2} -> 
+  [first_item, second_item] = String.split(line, " ", trim: true)
+  {first_item_integer, _} = Integer.parse(first_item)
+  {second_item_integer, _} = Integer.parse(second_item)
+  {[first_item_integer | list1], [second_item_integer | list2]}
+end)
+
+# Sort the lists
+sorted_list1 = Enum.sort(list1)
+sorted_list2 = Enum.sort(list2)
+
+# Calculate the total distance
+total_distance =
+  Enum.zip(sorted_list1, sorted_list2)
+  |> Enum.reduce(0, fn {item1, item2}, acc ->  
+    acc + abs(item1 - item2)
+  end)
+
+IO.puts(total_distance)
---
+++ 1/README.md
@@ -0,0 +1,12 @@
+# Notes on solution
+
+## Part 1
+
+### Assumptions
+- Both lists are the same length.
+
+### Method to solve
+The way I'm planning on going about solving this is by:
+1. Loading both lists and sorting them in ascending order.
+1. Look at the 0th through Nth item of each item in the list
+1. Take the absolute value difference of each item and add to a running sum
